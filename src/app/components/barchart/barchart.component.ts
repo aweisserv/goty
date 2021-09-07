124996cb23398936906a915f11034059064effff
Allan Weisser Verdessi <vredes@live.com>
2021-09-07 00:31:37 -0400

    ngx chart horizontal and responsive working

--- src/app/components/barchart/barchart.component.ts
+++ src/app/components/barchart/barchart.component.ts
@@ -7,8 +7,24 @@ import { Component } from '@angular/core';
 })
 export class BarchartComponent {
 
-  results!: any[];
-  view!: [700, 400];
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 25
+    },
+    {
+      "name": "Juego 3",
+      "value": 15
+    },
+    {
+      "name": "Juego 4",
+      "value": 10
+    }
+  ];
 
   // options
   showXAxis   = true;
