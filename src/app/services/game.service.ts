28d004e171ab920866422549a37913571c17d46a
Allan Weisser Verdessi <vredes@live.com>
2021-09-23 00:11:47 -0400

    system goty cache working

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -3,15 +3,36 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
 
+import { tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
 export class GameService {
+
+  games: Game[] = [];
 
   constructor( private http: HttpClient ) {   }
 
   getNominados() {
-    return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+
+    if( this.games.length > 0 ) {
+      // no hay juegos
+      console.log("Desde caché");
+      return of(this.games);
+
+
+    }else{
+      console.log("Desde internet");
+      return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+      .pipe(
+        tap(
+          games => this.games = games
+        )
+      );
+    }
+ 
   }
 
 
